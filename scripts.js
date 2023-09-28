8d1ff9c08dc410058d99ad76da4df057551b4561
KrystianRusin <krystian.r@hotmail.com>
2023-09-28 17:15:35 -0400

    created very basic calculator layout and added base operation choosing logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+const addButton = document.getElementById("addBtn");
+addButton.addEventListener('click', () => {
+    firstNumber = prompt("enter first number")
+    secondNumber = prompt("enter second number")
+    operate(firstNumber, secondNumber, "+")
+})
+
+const operate = (a, b, operatation) => {
+    if(operatation === "+"){
+        console.log(add(firstNumber, secondNumber))
+    }
+    if(operatation === "-"){
+        subtract(firstNumber, secondNumber)
+    }
+    if(operatation === "x"){
+        multiply(firstNumber, secondNumber)
+    }
+    if(operatation === "/"){
+        divide(firstNumber, secondNumber)
+    }
+}
+
+
 const add = (a, b) => {
-    return a + b
+    
+    return parseInt(a) + parseInt(b)
 }
 
 const subtract = (a,b) => {
