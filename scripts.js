a7dab2e22c6957862f7888427ad202e71b563a79
KrystianRusin <krystian.r@hotmail.com>
2023-09-28 20:02:41 -0400

    added basic display text for when user provides an input

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
 let firstNumber;
 let secondNumber;
 let operator;
+let result;
 
 const numbers = document.querySelectorAll(".number-input")
 numbers.forEach(number => number.addEventListener('click', (event) => {
     if(operator){
         if(secondNumber){
             secondNumber += event.target.id
+            printToDisplay(event.target.id)
         }else {
             secondNumber = event.target.id
+            printToDisplay(event.target.id)
         }
     } else {
         if(firstNumber){
             firstNumber += event.target.id
+            printToDisplay(event.target.id)
         } else {
             firstNumber = event.target.id
+            printToDisplay(event.target.id)
         }
         
     }
@@ -23,46 +28,56 @@ numbers.forEach(number => number.addEventListener('click', (event) => {
 const operations = document.querySelectorAll(".operation-input")
 operations.forEach(operation => operation.addEventListener('click', (event) => {
     operator = event.target.id
+    printToDisplay(operator)
 }))
 
 const operate = (a, b, operatation) => {
     if(operatation === "+"){
-        console.log(add(firstNumber, secondNumber))
+        result = add(firstNumber, secondNumber)
     }
     if(operatation === "-"){
-        console.log(subtract(firstNumber, secondNumber))
+        result = subtract(firstNumber, secondNumber)
     }
     if(operatation === "X"){
-        console.log(multiply(firstNumber, secondNumber))
+        result = multiply(firstNumber, secondNumber)
     }
     if(operatation === "/"){
-        console.log(divide(firstNumber, secondNumber))
+        result = divide(firstNumber, secondNumber)
     }
 }
+
+const display = document.getElementById('display')
+const printToDisplay = (userInput) => {
+    // const div = document.createElement('div')
+    // div.
+    display.textContent += userInput
+} 
 
 
 const add = (a, b) => {
     
-    return parseInt(a) + parseInt(b)
+    return parseFloat(a) + parseFloat(b)
 }
 
 const subtract = (a,b) => {
-    return parseInt(a) - parseInt(b)
+    return parseFloat(a) - parseFloat(b)
 }
 
 const multiply = (a, b) => {
-    return parseInt(a)*parseInt(b)
+    return parseFloat(a)*parseFloat(b)
 }
 
 const divide = (a,b) => {
-    return parseInt(a)/parseInt(b)
+    return parseFloat(a)/parseFloat(b)
 }
 
 const equals = document.getElementById("=");
 equals.addEventListener('click', () => {
     operate(firstNumber, secondNumber, operator)
+    printToDisplay(" = " + result)
     firstNumber = null;
     secondNumber = null;
+    result = firstNumber
     operator = null;
 })
 
@@ -71,4 +86,5 @@ clearBtn.addEventListener('click', () => {
     firstNumber = null;
     secondNumber = null;
     operator = null;
+    display.textContent = ""
 })
